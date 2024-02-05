85605237d4ae088135bd6c308ef8f33e02ca85b6
Pedro <pedrojuniorifpb@gmail.com>
2024-02-05 18:14:00 -0300

    Adicionando mais testes para login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,10 @@
 import { v4 as uuidv4 } from "uuid";
 
+const email = `${uuidv4()}@gmail.com`;
+
 describe("Testes envolvendo realizar login", () => {
-    beforeEach(() => {
+    before(() => {
         cy.visit("/");
-    });
-
-    it("Realizando login com sucesso", () => {
-        const email = `${uuidv4()}@gmail.com`;
-
         cy.intercept({ method: "POST" }).as("methodPost");
 
         cy.get(`[data-cy="login-redirect_register"]`).click();
@@ -15,11 +12,35 @@ describe("Testes envolvendo realizar login", () => {
 
         cy.wait("@methodPost").its("response.statusCode").should("eq", 201);
         cy.wait(500);
+    });
 
-        // cy.get(`[data-cy="register-redirect_login"]`).click();
+    beforeEach(() => {
+        cy.visit("/");
+        cy.intercept({ method: "POST" }).as("methodPost");
+    });
+
+    it("Realizando login com sucesso", () => {
         cy.login(email);
-
         cy.wait("@methodPost").its("response.statusCode").should("eq", 201);
         cy.wait(500);
+    });
+
+    it("Verificando se mensagens de error estão corretas", () => {
+        const wrongEmail = `${uuidv4()}@hortmail.com`;
+        cy.login(wrongEmail);
+        cy.wait("@methodPost").its("response.statusCode").should("not.eq", 201);
+        cy.wait(500);
+        cy.get("p.text-tertiary").each((element) =>
+            cy.wrap(element).should("be.visible")
+        );
+    });
+
+    it("Deslogando com sucesso após realizar login", () => {
+        cy.login(email);
+        cy.wait("@methodPost").its("response.statusCode").should("eq", 201);
+        cy.wait(500);
+
+        cy.get(`[data-cy="logout"]`).click();
+        cy.url().should("include", "/login");
     });
 });
